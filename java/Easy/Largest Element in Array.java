ac6f3a42e0b0bac9285e36b1700984635d54d25f
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-08-31 11:05:41 +0530

    Second commit

---
+++ java/Easy/Largest Element in Array.java
@@ -0,0 +1,11 @@
+class Compute {
+    
+    public long largest(long arr[], long n)
+    {
+       int max=(int)arr[0];
+       for(int i=1;i<n;i++){
+           if(arr[i]>max) max=(int)arr[i];
+       }
+       return max;
+    }
+}
