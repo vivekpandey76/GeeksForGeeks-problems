01e39dd42e6ae07dc0bc29b4b6f5a90d6cc1a812
Vivek pandey <74551457+vivekpandey76@users.noreply.github.com>
2021-09-05 10:56:50 +0530

    Create Sum of Array Elements .java

---
+++ java/Easy/Sum of Array Elements .java
@@ -0,0 +1,11 @@
+class Get
+{
+    public static int sumElement(int arr[], int n)
+    {
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+}
